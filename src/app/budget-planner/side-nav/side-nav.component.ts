41d590126768251151b6ef8400d6989135424dec
lucas-pettinelli <pettinellilucas@gmail.com>
2024-03-30 21:28:53 -0300

    feat: adding login, side nav components and configuring angular material theme

--- src/app/budget-planner/side-nav/side-nav.component.ts
+++ src/app/budget-planner/side-nav/side-nav.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
 export class SideNavComponent {
 
+  isSlideOut = true;
+
+  toggleSlideOut():void{
+    this.isSlideOut = !this.isSlideOut;
+  }
+
+  constructor(private router: Router) { }
+
+
+  onDash() {
+    this.router.navigate(['/budget-planner/dashboard']);
+  }
+
+  onProfile() {
+    this.router.navigate(['/budget-planner/profile']);
+  }
+
+  onHistory() {
+    this.router.navigate(['/budget-planner/history']);
+  }
+
+  onLogout() {
+    this.router.navigate(['/budget-planner/login']);
+  }
 }
